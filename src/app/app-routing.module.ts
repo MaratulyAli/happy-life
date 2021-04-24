368a2e5b0566a4064ba7f5ca73500bd62c6f59a4
maratulyali <maratulyali01@gmail.com>
2021-04-24 23:01:19 +0500

    Add auth guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,16 @@ import { LoginComponent } from './login/login.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { PeriodicalComponent } from './periodical/periodical.component';
 import { PeriodicalsComponent } from './periodicals/periodicals.component';
+import { redirectUnauthorizedTo, redirectLoggedInTo, canActivate } from '@angular/fire/auth-guard';
+
+const redirectUnauthorizedToLogin = () => redirectUnauthorizedTo(['login']);
+const redirectLoggedInToPeriodicals = () => redirectLoggedInTo(['periodicals']);
 
 const routes: Routes = [
   { path: '', redirectTo: 'periodicals', pathMatch: 'full' },
-  { path: 'periodical/:id', component: PeriodicalComponent },
-  { path: 'periodicals', component: PeriodicalsComponent },
-  { path: 'login', component: LoginComponent },
+  { path: 'periodicals', component: PeriodicalsComponent, ...canActivate(redirectUnauthorizedToLogin) },
+  { path: 'periodical/:id', component: PeriodicalComponent, ...canActivate(redirectUnauthorizedToLogin) },
+  { path: 'login', component: LoginComponent, ...canActivate(redirectLoggedInToPeriodicals) },
   { path: '**', component: PageNotFoundComponent },
 ];
 
