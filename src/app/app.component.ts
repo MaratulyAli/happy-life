715529cabc9750fa20a3d83bbafa0647f24557fb
maratulyali <maratulyali01@gmail.com>
2021-04-26 06:48:30 +0500

    Check role

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,35 @@
 import { HttpErrorResponse } from '@angular/common/http';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
+import { AngularFirestore } from '@angular/fire/firestore';
 import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { first, map, tap } from 'rxjs/operators';
+import { IUser } from './_shared/models/user.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   opened = false;
+  role$!: Observable<string | undefined>;
 
   constructor(
     public auth: AngularFireAuth,
+    public fs: AngularFirestore,
     public router: Router,
   ) {
+  }
+
+  async ngOnInit(): Promise<void> {
+    const user = await this.auth.user.pipe(first()).toPromise();
+    this.role$ = this.fs.doc<IUser>(`users/${user?.uid}`)
+      .valueChanges()
+      .pipe(
+        map(u => u?.role)
+      );
   }
 
   public async logout(): Promise<void> {
